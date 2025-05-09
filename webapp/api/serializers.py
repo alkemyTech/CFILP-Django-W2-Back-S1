25671920e7f02a71e9f7e6e63f6ca9483702da7f
Soraya <soryviolini@gmail.com>
2025-05-09 04:10:10 -0300

    Implementación CRUD de servicios y carga de datos inicial

--- webapp/api/serializers.py
+++ webapp/api/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-
-from servicios.models import Cliente
+from servicios.models import Cliente, Servicio, Reserva
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +10,24 @@ class ClienteSerializer(serializers.ModelSerializer):
             'apellido',
             'activo'
         ]
+        
+class ServicioSerializer(serializers.ModelSerializer):
+    """Serializador para el modelo Servicio.
+    Convierte instancias a JSON y valida entradas."""
+    
+    class Meta:
+        model = Servicio
+        fields = '__all__'
+        
+class ReservaSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        # Validación de integridad. Las reservan se asocian sólo con activos
+        entidades = ['servicio']
+        for campo in entidades:
+            if not data[campo].activo:
+                raise serializers.ValidationError(f"{campo.capitalize()} inactivo.")
+        return data
+
+    class Meta:
+        model = Reserva
+        fields = '__all__'
--- webapp/api/views.py
+++ webapp/api/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from servicios.models import Cliente
-from api.serializers import ClienteSerializer
-
-from rest_framework.generics import (ListAPIView,)
+from servicios.models import Cliente, Servicio, Reserva
+from api.serializers import ClienteSerializer, ServicioSerializer, ReservaSerializer
+from rest_framework.generics import (ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView)
 
 #region ---- CRUD CLIENTE ----
 
@@ -13,3 +12,63 @@ class ClienteListApiView(ListAPIView):
         return Cliente.objects.all()
 
 #endregion
+
+# region ----- CRUD para Servicios
+class ServicioListApiView(ListAPIView):
+    serializer_class = ServicioSerializer
+    
+    def get_queryset(self):
+        return Servicio.objects.filter(activo=True) 
+
+class ServicioDetailApiView(RetrieveAPIView):
+    serializer_class = ServicioSerializer
+    
+    def get_queryset(self):
+        return Servicio.objects.filter(activo=True)
+
+class ServicioCreateApiView(CreateAPIView):
+    serializer_class = ServicioSerializer
+    
+    def get_queryset(self):
+        return Servicio.objects.all()
+
+class ServicioUpdateApiView(UpdateAPIView):
+    serializer_class = ServicioSerializer
+    
+    def get_queryset(self):
+        return Servicio.objects.filter(activo=True)
+
+class ServicioDeleteApiView(DestroyAPIView):
+    serializer_class = ServicioSerializer
+    
+    def get_queryset(self):
+        return Servicio.objects.filter(activo=True)
+    
+    def perform_destroy(self, instance):
+        # Baja lógica
+        instance.activo = False
+        instance.save()
+        
+# endregion
+        
+# region ------ CRUD para reservas
+        
+class ReservaListApiView(ListAPIView):
+    serializer_class = ReservaSerializer
+    
+    def get_queryset(self):
+        return Reserva.objects.all()
+
+class ReservaDetailApiView(RetrieveAPIView):
+    serializer_class = ReservaSerializer
+    
+    def get_queryset(self):
+        return Reserva.objects.all()
+
+class ReservaCreateApiView(CreateAPIView):
+    serializer_class = ReservaSerializer
+    
+    def get_queryset(self):
+        return Reserva.objects.all()
+    
+# endregion
